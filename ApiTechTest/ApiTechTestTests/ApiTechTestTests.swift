3d59122271ee7a8e6ce95868bbdeb8557a22f332
abid <gameplaycoder@gmail.com>
2018-06-18 10:18:32 +0100

    corrected the products returned test

--- ApiTechTest/ApiTechTestTests/ApiTechTestTests.swift
+++ ApiTechTest/ApiTechTestTests/ApiTechTestTests.swift
@@ -61,4 +61,55 @@ class ApiTechTestTests: XCTestCase {
         // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
         wait(for: [expectation], timeout: 10.0)
     }
+    
+    func testServerShouldReturnListOfProductsGivenProductsPerPage()
+    {
+        
+        // Create an expectation for a background download task.
+        let expectation = XCTestExpectation(description: "Download products from server")
+        
+        if var urlComponents = URLComponents(string: ClientApi.baseUrl) {//
+            
+            urlComponents.query = "q=dishwasher&key=\(ClientApi.apiKey)&pageSize=20"
+            
+            let dataTask = urlSession.dataTask(with: urlComponents.url!) { data, response, error in
+                
+                if let errorReturn = error {
+                    
+                    print("url error: \(errorReturn.localizedDescription)")
+                    
+                }
+                else if data != nil {
+                    // no error
+                    
+                    if let resp = response as? HTTPURLResponse {
+                        
+                        if resp.statusCode == 200 {
+                            
+                            
+                            let jsonDic = try? JSONSerialization.jsonObject(with: data!) as? [String:Any]
+                            
+                            if let productsDict = jsonDic!!["products"] as? [Any] {
+                            
+                                // Make sure we downloaded some data.
+                                XCTAssertTrue(productsDict.count > 0 , "No products in response")
+                                
+                                // Fulfill the expectation to indicate that the background task has finished successfully.
+                                expectation.fulfill()
+                                
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            dataTask.resume()
+        }
+        
+        // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+        wait(for: [expectation], timeout: 10.0)
+        
+    }
+    
+    
 }
